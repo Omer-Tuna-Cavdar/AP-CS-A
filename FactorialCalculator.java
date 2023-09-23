0c8c938618e2ec2216270eece670aeb785fd7b8b
Omer Tuna Çavdar <143618537+Omer-Tuna-Cavdar@users.noreply.github.com>
2023-09-23 20:39:52 +0000

    25 Lines of Code

--- FactorialCalculator.java
+++ FactorialCalculator.java
@@ -1,23 +1,26 @@
 // This is a Java program that calculates the factorial of a number
-
 import java.util.Scanner;  // Import the Scanner class for user input
-
 public class FactorialCalculator {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
-        
-        System.out.print("Enter a number: ");  // Prompt the user for input
-        int number = scanner.nextInt();  // Read an integer from the user
-        
-        int factorial = 1;  // Initialize factorial to 1
-        
-        // Calculate the factorial using a loop
-        for (int i = 1; i <= number; i++) {
+    private int factorial = 1;  // Initialize factorial to 1
+    public int factorial(int number) {
+        // Calculate the factorial using this method
+     for (int i = 1; i <= number; i++) {
             factorial *= i;  // Multiply factorial by i
         }
-        
-        System.out.println("Factorial of " + number + " is: " + factorial);  // Print the result
-        
-        scanner.close();  // Close the Scanner
+        return factorial;
+    }
+    public int maxvaluesforthisprogram() {
+        return Integer.MAX_VALUE;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        System.out.print("Enter a number: ");  // Prompt the user for input
+        int number = scanner.nextInt();  // Read user input
+        scanner.close();  // Close the Scanner object
+        System.out.println("You entered: " + number);  // Output user input
+        FactorialCalculator calculator = new FactorialCalculator();
+        System.out.println("The factorial of " + number + " is " + calculator.factorial(number));
+        System.out.println("The maximum value for this program is " + calculator.maxvaluesforthisprogram());
     }
 }
